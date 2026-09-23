141e5d7d85acdfd2b00a9c8fbcd6dd5a871d57f9
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-09-23 08:36:19 -0300

    add - validação cadastro usuarios

--- public/cadastrar_usuarios.php
+++ public/cadastrar_usuarios.php
@@ -7,13 +7,26 @@ $usuario = $_POST["usuario"];
 $senha = $_POST["senha"];
 $email = $_POST["email"];
 
-if(empty($nome) || empty($usuario) || empty($senha) || empty($email)) {
-    echo"<script>
-        alert('Preencha todos os campos!');
-        history.back();
+if(mb_strlen($nome) < 3 ||  !preg_match('/^[\p{L}]+$/u', $nome)){
+    echo "<script>alert('Nome inválido! O nome deve conter apenas letras e no mínimo 3');
+    window.location.href = 'cadastro_usuarios.php?id=$id';
     </script>";
-
-    exit();
+    exit;
+} else if(strlen($usuario) < 5){
+    echo "<script>alert('Usuário inválido! O nome deve conter pelo menos 5 caracteres');
+    window.location.href = 'cadastro_usuarios.php?id=$id';
+    </script>";
+    exit;
+} else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    echo "<script>alert('E-mail inválido! Insira um E-mail válido');
+    window.location.href = 'cadastro_usuarios.php?id=$id';
+    </script>";
+    exit;
+}else if(strlen($senha) < 8){
+    echo "<script>alert('Senha inválida! A senha deve conter no mínimo 8 caracteres');
+    window.location.href = 'cadastro_usuarios.php?id=$id';
+    </script>";
+    exit;
 }
 
 $senha = password_hash($senha, PASSWORD_DEFAULT);
